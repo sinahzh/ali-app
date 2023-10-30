3d2a00873921bc6c31f6ec9922be84defea99be4
sinahzh <hzhsina@gmail.com>
2023-10-30 22:04:52 +0330

    feat(): add menu in component main and it is not compelete

--- src/presentation/app/clients/clients.module.ts
+++ src/presentation/app/clients/clients.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { ClientsComponent } from './clients.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    ClientsComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    ClientsComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ClientsModule { }
